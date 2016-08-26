0b5031c9e037e8b1ed737a693870d1131cc5fb68
Guy Pursey <guypursey@gmail.com>
2016-08-26 17:30:12 +0100

    Added simple tests for function.

---
+++ test/simple-parse.js
@@ -0,0 +1,42 @@
+"use strict"
+const dd_path = "../index.js"
+const dickiedates = require(dd_path)
+const expect = require("chai").expect
+
+describe("Using `dickie-dates`", function() {
+	describe("to parse a simple string", function () {
+		describe("like `198` to represent the 1980s", function () {
+
+			let result = dickiedates("198")
+
+			it("should return an object", function () {
+				expect(result).to.be.an("object")
+			})
+
+			it("should return an object with a `startDate` property", function () {
+				expect(result).to.have.property("startDate")
+			})
+
+			it("should return an object with an `endDate` property", function () {
+				expect(result).to.have.property("endDate")
+			})
+
+			it("should return an object with a `precision` property", function () {
+				expect(result).to.have.property("startDate")
+			})
+
+			it("should return an object with a `startDate` value of '19800101000000'", function () {
+				expect(result).to.have.property("startDate", "19800101000000")
+			})
+
+			it("should return an object with a `endDate` value of '19891231235959'", function () {
+				expect(result).to.have.property("endDate", "19891231235959")
+			})
+
+			it("should return an object with a `precision` value of '2' (out of 12)", function () {
+				expect(result).to.have.property("precision", 2)
+			})
+
+		})
+	})
+})
