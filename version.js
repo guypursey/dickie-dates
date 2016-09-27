a7554ae3bb01f2fe34bcd925759fe7d2ec2ef834
Guy Pursey <guypursey@gmail.com>
2016-09-27 17:27:09 +0100

    Corrected regex code in version.js.

--- version.js
+++ version.js
@@ -2,7 +2,7 @@ var fs = require("fs")
 
 fs.readFile("README.md", "utf8", function (error, data) {
 	if (error) throw error
-	var bumped = data.replace(/(\d+\.\d+\.\d)/g, process.argv[2] || "$1")
+	var bumped = data.replace(/(\d+\.\d+\.\d+)/g, process.argv[2] || "$1")
 	fs.writeFile("README.md", bumped, function (error) {
 		if (error) throw error
 		console.log(process.argv[2])
